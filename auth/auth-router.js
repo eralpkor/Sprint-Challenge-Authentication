fbb7d8f45f69dbb50a2f1ab1e77fd7521a203245
eralpkor <eralp.ny@gmail.com>
2019-10-18 11:35:25 -0400

    fixed package.json and index.js

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
+
+  
 });
 
 router.post('/login', (req, res) => {
