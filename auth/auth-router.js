5a156d23a16ff78150c1e092d2cdef5a51bcd787
eralpkor <eralp.ny@gmail.com>
2019-10-18 14:05:58 -0400

    auth-router tests added

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const secrets = require('../config/secrets');
 const Users = require('./auth-model');
 
 router.post('/register', (req, res) => {
@@ -53,7 +54,7 @@ function generateToken(user) {
   const options = {
     expiresIn: '1d',
   };
-  return jwt.sign(payload, process.env.JWT_SECRET, options)
+  return jwt.sign(payload, secrets.jwtSecret, options)
 }
 
 
--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -1,5 +1,5 @@
 const supertest = require('supertest');
-const server = require('./auth-router.js');
+const server = require('../api/server');
 const db = require('../database/dbConfig');
 
 describe('/register end point', () => {
@@ -8,6 +8,48 @@ describe('/register end point', () => {
   });
 
   it('should return 201, registration', async () => {
+    const authorization = await supertest(server)
+      .post('/api/auth/register')
+      .send({ username: 'sunny', password: 'pass' })
+
+    expect(authorization.status).toBe(201)
     
   });
+
+  it('should return json when registered', async () => {
+    const auth = await supertest(server)
+      .post('/api/auth/register')
+      .send({ username: 'sunny', password: 'pass' });
+
+    expect(auth.type).toMatch(/json/i);
+  });
 });
+
+// api/auth/login
+describe('POST, /api/auth/login end point', () => {
+  it('should return 200 when logged in', async () => {
+    await supertest(server)
+      .post('/api/auth/register')
+      .send({ username: 'sunny', password: 'pass' });
+
+    const auth = await supertest(server)
+      .post('/api/auth/login')
+      .send({username: 'sunny', password: 'pass' });
+
+    expect(auth.status).toBe(200);
+  });
+
+  it('should return json when logged in', async () => {
+    await supertest(server)
+      .post('/api/auth/register')
+      .send({ username: 'sunny', password: 'pass' });
+
+    const auth = await supertest(server)
+      .post('/api/auth/login')
+      .send({ username: 'sunny', password: 'pass' });
+
+    expect(auth.type).toMatch(/json/i);
+  });
+});
+  
+
